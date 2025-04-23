d6f156456c2516188faaf40b74e63407a328bab0
ronaldo <ronaldo.dsantos@gmail.com>
2025-04-22 23:33:15 -0300

    criado estrutura do projeto

---
+++ src/EmpresaCadastroApp.Domain/Entities/Company.cs
@@ -0,0 +1,26 @@
+﻿namespace EmpresaCadastroApp.Domain.Entities
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string? Cnpj { get; set; }
+        public string? NomeEmpresarial { get; set; }
+        public string? NomeFantasia { get; set; }
+        public string? Situacao { get; set; }
+        public string? Abertura { get; set; }
+        public string? Tipo { get; set; }
+        public string? NaturezaJuridica { get; set; }
+        public string? AtividadePrincipal { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Municipio { get; set; }
+        public string? Uf { get; set; }
+        public string? Cep { get; set; }
+
+        // Relacionamento
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ src/EmpresaCadastroApp.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmpresaCadastroApp.Domain.Entities
+{
+    public class User : IdentityUser<Guid>
+    {
+        public string? Nome { get; set; }
+
+        // Navegação
+        public ICollection<Company>? Companies { get; set; }
+    }
+}
