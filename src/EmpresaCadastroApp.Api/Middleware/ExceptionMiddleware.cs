c6658de92f4f8a08d47577e1a57c877a01bcbadf
ronaldo <ronaldo.dsantos@gmail.com>
2025-04-25 00:36:16 -0300

    adicionado exception middleware

---
+++ src/EmpresaCadastroApp.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using EmpresaCadastroApp.Application.Utils;
+using System.Net;
+using System.Text.Json;
+
+namespace EmpresaCadastroApp.Api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var result = Result<string>.Fail("Ocorreu um erro inesperado. Tente novamente mais tarde.");
+
+            var json = JsonSerializer.Serialize(result);
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
--- src/EmpresaCadastroApp.Api/Program.cs
+++ src/EmpresaCadastroApp.Api/Program.cs
@@ -1,4 +1,5 @@
 using EmpresaCadastroApp.Api.Configuration;
+using EmpresaCadastroApp.Api.Middleware;
 using EmpresaCadastroApp.Application.Mappings;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,6 +23,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthentication();
 
